200615d4f3352d31a22447e70254b33ce5b156b1
Vehicle Researcher <user@comma.ai>
2022-04-07 13:41:09 -0500

    openpilot v0.8.14-cbl-db8214ab-2022-04-07T04:34:21-69ecc384-2022-04-07T05:04:01 release

---
+++ __SCRIPTS__/restartOP.sh
@@ -0,0 +1,1 @@
+tmux kill-session -t comma; rm -f /tmp/safe_staging_overlay.lock; tmux new -s comma -d "echo $$ > /dev/cpuset/app/tasks && echo $PPID > /dev/cpuset/app/tasks && /data/openpilot/launch_openpilot.sh"
