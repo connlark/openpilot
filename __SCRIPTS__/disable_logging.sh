2184dbd5b73296c9fe77fa8431384fe50bf7487b
Connor Larkin <connor@connorlarkin.com>
2022-04-07 04:25:45 -0500

    fix

--- __SCRIPTS__/disable_logging.sh
+++ __SCRIPTS__/disable_logging.sh
@@ -1,4 +1,1 @@
-sed -i '' 's/, DaemonProcess/ #, DaemonProcess/g' selfdrive/manager/process_config.py
-sed -i '' 's/  DaemonProcess/#  DaemonProcess' selfdrive/manager/process_config.py
-sed -i '' 's/  NativeProcess/#  NativeProcess/g' selfdrive/manager/process_config.py
-sed -i '' 's/  PythonProcess/#  PythonProcess/g' selfdrive/manager/process_config.py
+git checkout origin/master -- selfdrive/manager/process_config.py
--- __SCRIPTS__/enable_logging.sh
+++ __SCRIPTS__/enable_logging.sh
@@ -1,1 +1,4 @@
-git checkout origin/master -- selfdrive/manager/process_config.py
+sed -i '' 's/ #, DaemonProcess/, DaemonProcess/g' selfdrive/manager/process_config.py
+sed -i '' 's/#  DaemonProcess/  DaemonProcess' selfdrive/manager/process_config.py
+sed -i '' 's/#  NativeProcess/  NativeProcess/g' selfdrive/manager/process_config.py
+sed -i '' 's/#  PythonProcess/  PythonProcess/g' selfdrive/manager/process_config.py
