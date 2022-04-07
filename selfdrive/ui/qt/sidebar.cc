ba1d0b747634cb6f0f7c0d9f2f4359889a46cb8d
Connor Larkin <connor@connorlarkin.com>
2022-04-07 03:09:24 -0500

    updates

--- selfdrive/ui/qt/sidebar.cc
+++ selfdrive/ui/qt/sidebar.cc
@@ -64,7 +64,7 @@ void Sidebar::updateState(const UIState &s) {
   setProperty("connectStatus", QVariant::fromValue(connectStatus));
 
   //cbl
-  QString ambientTemp =  QString("%1°C").arg((int)deviceState.getAmbientTempC());
+  QString ambientTemp =  QString("%1°C\n").arg((int)deviceState.getAmbientTempC());
 
   ItemStatus tempStatus = {ambientTemp, danger_color};
   auto ts = deviceState.getThermalStatus();
