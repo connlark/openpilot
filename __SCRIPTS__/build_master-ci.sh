69ecc3849448f0ffdcda006bdf6153cc57300200
Connor Larkin <connor@connorlarkin.com>
2022-04-07 05:03:42 -0500

    updates

--- __SCRIPTS__/build_master-ci.sh
+++ __SCRIPTS__/build_master-ci.sh
@@ -1,5 +1,5 @@
 pushd release;
 
-PUSH='master-ci' EXTRA_FILES="__SCRIPTS__/disable_logging.sh __SCRIPTS__/enable_logging.sh __SCRIPTS__/restartOP.sh"./build_devel.sh
+PUSH='master-ci' EXTRA_FILES="__SCRIPTS__/disable_logging.sh __SCRIPTS__/enable_logging.sh __SCRIPTS__/restartOP.sh" ./build_devel.sh
 
 popd;
